114b8e5d531e3d5d7ec4dc261e4c26547a50f39f
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-04-12 12:08:58 +0300

    12.04.2023 12:08

--- my-app/src/Components/mainContent/Lessons/Components Lessons/LessonsObjects/TodoLessonsListItem.tsx
+++ my-app/src/Components/mainContent/Lessons/Components Lessons/LessonsObjects/TodoLessonsListItem.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import {useAppDispatch} from "../../../../../hooks/hooks";
 import {completedTodo, deleteTodo} from "../../../../../redux/Slices/lessonsObjectTodoSlice";
+import style from './TodoLessons.module.css'
 
 
 type TodoProps = {
@@ -9,7 +10,7 @@ type TodoProps = {
     title: string,
 }
 
-const TodoLessonsListItem: FC <TodoProps> = ({id, completed, title}) => {
+const TodoLessonsListItem: FC<TodoProps> = ({id, completed, title}) => {
     const dispatch = useAppDispatch()
 
     const deleteHandler = () => {
@@ -19,15 +20,32 @@ const TodoLessonsListItem: FC <TodoProps> = ({id, completed, title}) => {
         }
     }
 
+    const editHandler = () => {
+        const edit = window.confirm('edit todo?')
+        if (edit) {
+
+        }
+    }
+
     const completedHandler = () => {
         dispatch(completedTodo(id))
     }
 
     return (
-        <div>
+        <div className={style.Todo}>
             <input type={"checkbox"} checked={completed} onChange={completedHandler}/>
-            {title}
-            <button onClick={deleteHandler}>&times;</button>
+
+            <span>
+                {title}
+            </span>
+            {' '}
+            <button className={style.TodoButton}>
+                <img src="https://cdn-icons-png.flaticon.com/512/1827/1827951.png" alt=""/>
+            </button>
+            {' '}
+            <button className={style.TodoButton} onClick={deleteHandler}>
+                &times;
+            </button>
         </div>
     );
 };

2478c03e31c65f7a21657d07e673c7b88717752f
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-04-22 11:10:35 +0300

    22.04.2023

--- my-app/src/Components/Header/Header.tsx
+++ my-app/src/Components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import style from './Header.module.css'
+import Menu from "./Header componnets/Menu";
 
 const Header = () => {
     return (
         <div className={style.item}>
+
                 <input className={style.inputHeader} placeholder={'add text...'} />
                 <button className={style.search}><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/OOjs_UI_icon_search-ltr.svg/1200px-OOjs_UI_icon_search-ltr.svg.png" alt=""/></button>
+                    <Menu/>
         </div>
     );
 };

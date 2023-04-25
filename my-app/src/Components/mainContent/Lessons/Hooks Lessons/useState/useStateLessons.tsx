a1150bea6249cccceab1406419a85b8fd33c95a4
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-04-25 12:21:41 +0300

    25.04.2023

--- my-app/src/Components/mainContent/Lessons/Hooks Lessons/useState/useStateLessons.tsx
+++ my-app/src/Components/mainContent/Lessons/Hooks Lessons/useState/useStateLessons.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const UseStateLessons = () => {
+    const [count, setCount] = useState(0)
+
+    const handlerPlus = () => {
+        setCount(count + 1)
+    }
+
+    const handlerMinus = () => {
+        setCount(count - 1)
+    }
     return (
         <div>
-            useState
+            <h3>first example:</h3>
+            Counter: {count}
+            <li>
+                <button onClick={handlerMinus}>- Minus</button>
+                <button onClick={handlerPlus}> Plus+</button>
+            </li>
+
+            <h3>second example:</h3>
+
         </div>
     );
 };

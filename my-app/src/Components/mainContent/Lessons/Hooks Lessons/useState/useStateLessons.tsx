c83322ed79b3d1093617a94a486c109a804c8f9f
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-04-30 21:08:00 +0300

    30.04.2023 rtk

--- my-app/src/Components/mainContent/Lessons/Hooks Lessons/useState/useStateLessons.tsx
+++ my-app/src/Components/mainContent/Lessons/Hooks Lessons/useState/useStateLessons.tsx
@@ -5,31 +5,38 @@ import Todo from "./useStateLessonsComponnets/todo/Todo";
 const initialState = 0
 
 const UseStateLessons: FC = () => {
-    const [count, setCount] = useState(initialState)
-
-    const handlerPlus = () => {
+  const [count, setCount] = useState(initialState)
+    const minusHandler = () => {
+        setCount(count - 1)
+        if (count <= -5) {
+            if (window.confirm('Minimum -5 Resets?')){
+                setCount(0)
+            }
+            else return setCount(-5)
+        }
+    }
+    const resetsHandler = () => {
+        setCount(0)
+    }
+    const plusHandler = () => {
         setCount(count + 1)
+        if (count >= 5) {
+            if (window.confirm('Maximum 5 Resets?')){
+                setCount(0)
+            }
+            else return setCount(5)
+        }
     }
 
-    const handlerMinus = () => {
-        setCount(prev => prev - 1)
-    }
-
-    const handlerReset = () => {
-        setCount(initialState)
-    }
     return (
         <div>
             <h3>first example:</h3>
-            Counter: {count}
-            <li>
-                <button onClick={handlerMinus}>- Minus</button>
-                {' '}
-                <button onClick={handlerReset}> reset</button>
-                {' '}
-                <button onClick={handlerPlus}> Plus+</button>
-
-            </li>
+            <h3>
+                {count}
+            </h3>
+            <button onClick={minusHandler}>- Minus</button>
+            <button onClick={resetsHandler}>Resets</button>
+            <button onClick={plusHandler}>Plus +</button>
 
             <h3>second example:</h3>
             <Todo/>

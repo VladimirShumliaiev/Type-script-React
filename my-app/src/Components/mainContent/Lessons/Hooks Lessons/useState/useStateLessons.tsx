ffe7ae63e0c3cbef69970ede7fc71efa932b7519
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-05-01 12:43:55 +0300

    01.05.2023 rtk

--- my-app/src/Components/mainContent/Lessons/Hooks Lessons/useState/useStateLessons.tsx
+++ my-app/src/Components/mainContent/Lessons/Hooks Lessons/useState/useStateLessons.tsx
@@ -5,14 +5,24 @@ import Todo from "./useStateLessonsComponnets/todo/Todo";
 const initialState = 0
 
 const UseStateLessons: FC = () => {
-  const [count, setCount] = useState(initialState)
+    const [count, setCount] = useState(initialState)
+
+    const str = 'aaa ' +
+        'bbb ' +
+        'ccc' +
+        ' dddd'
+
+    const str2 = `aaa 
+    bbb 
+    ccc 
+    ddd`
+
     const minusHandler = () => {
         setCount(count - 1)
         if (count <= -5) {
-            if (window.confirm('Minimum -5 Resets?')){
+            if (window.confirm('Minimum -5. Resets?')) {
                 setCount(0)
-            }
-            else return setCount(-5)
+            } else return setCount(count)
         }
     }
     const resetsHandler = () => {
@@ -21,10 +31,9 @@ const UseStateLessons: FC = () => {
     const plusHandler = () => {
         setCount(count + 1)
         if (count >= 5) {
-            if (window.confirm('Maximum 5 Resets?')){
+            if (window.confirm('Maximum 5. Resets?')) {
                 setCount(0)
-            }
-            else return setCount(5)
+            } else return setCount(count)
         }
     }
 

8943e4549c69af05635d318f7107ac8f51c7d012
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-05-11 10:06:24 +0300

    11.05.2023 +++

--- my-app/src/Components/mainContent/Lessons/Components Lessons/Posts/BlogPage.tsx
+++ my-app/src/Components/mainContent/Lessons/Components Lessons/Posts/BlogPage.tsx
@@ -2,27 +2,27 @@ import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 import styles from './Posts.module.css'
 
-type Post = {
+type Posts = {
     id: number
     title: string
 }
 
 const BlogPage = () => {
-    const [posts, setPosts] = useState<Post[]>([])
+    const [posts, setPosts] = useState<Posts[]>([])
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(res => res.json())
             .then(date => setPosts(date))
-    })
+    },[])
+
     return (
         <div className={styles.item}>
             {
                 posts.map(post => (
-                  <Link key={post.id} to={`/lessons/posts/${post.id}`}>
+                    <Link key={post.id} to={`/lessons/posts/${post.id}`}>
                         <li>{post.title}</li>
-                      <pre/>
-                  </Link>
+                    </Link>
                 ))
             }
         </div>
--- my-app/src/Components/mainContent/Lessons/Components Lessons/Posts/SinglePage.tsx
+++ my-app/src/Components/mainContent/Lessons/Components Lessons/Posts/SinglePage.tsx
@@ -1,29 +1,42 @@
 import React, {useEffect, useState} from 'react';
-import {useParams} from "react-router-dom";
+import {Link, Route, Routes, useParams} from "react-router-dom";
+import BlogPage from "./BlogPage";
+import styles from './Posts.module.css'
 
-type Post = {
+
+type Posts = {
     title: string
     body: string
 }
 
 const SinglePage = () => {
     const {id} = useParams()
-    const [posts, setPosts] = useState<Post>()
+    const [posts, setPosts] = useState<Posts>()
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(res => res.json())
             .then(date => setPosts(date))
     }, [id])
+
     return (
-        <div>
+        <div className={styles.item}>
             {posts && (
                 <>
                     <h3>{posts.title}</h3>
                     <p>{posts.body}</p>
+
+                    <Link to={'posts'}>
+                        back to page
+                    </Link>
+
+                    <div className={styles.itemBackToPage}>
+                        <Routes>
+                            <Route path={'posts'} element={<BlogPage/>}/>
+                        </Routes>
+                    </div>
                 </>
             )}
-
         </div>
     );
 };

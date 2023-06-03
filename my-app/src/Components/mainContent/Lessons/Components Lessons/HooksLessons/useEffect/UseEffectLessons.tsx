2576775e39c61cd2fe492894808679c4519524fd
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-06-03 21:12:09 +0300

    03.06.2023 +++

--- my-app/src/Components/mainContent/Lessons/Components Lessons/HooksLessons/useEffect/UseEffectLessons.tsx
+++ my-app/src/Components/mainContent/Lessons/Components Lessons/HooksLessons/useEffect/UseEffectLessons.tsx
@@ -8,25 +8,46 @@ type Comments = {
 }
 
 const UseEffectLessons: FC = () => {
-    const [value, setValue] = useState<Comments[]>([{id: '150', body: '155',name: 'asdasd', userId: '12323'}])
+    const [value, setValue] = useState<Comments[]>([{id: '150', body: '155', name: 'asdasd', userId: '12323'}])
+    const [checked, setChecked] = useState(false)
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/comments')
+        fetch('https://jsonplaceholder.typicode.com/comments/?_limit=5')
             .then(res => res.json())
             .then(date => setValue(date))
-    },[])
+    }, [])
+
+    useEffect(() => {
+        alert(`checked: ${checked.toString()}`)
+    }, [])
+
+    useEffect(() => {
+        console.log(checked ? 'Yes, checked' : 'No, not checked')
+    },[checked])
+
+
     return (
         <div>
             {value.map(e => (
                 <div key={e.id}>
-                   <div> id: {e.id}</div>
-                   <div> userId: {e.userId}</div>
-                   <div>name: {e.name}</div>
-                   <div>body: {e.body}</div>
+                    <div> id: {e.id}</div>
+                    <div> userId: {e.userId}</div>
+                    <div>name: {e.name}</div>
+                    <div>body: {e.body}</div>
                     <hr/>
                 </div>
             ))}
+
+            <div>
+                <input
+                    type="checkbox"
+                    checked={checked}
+                    onChange={() => setChecked(checked => !checked)}
+                />
+                {checked ? 'checked' : 'not checked'}
+            </div>
         </div>
     );
+
 };
 
 export default UseEffectLessons;

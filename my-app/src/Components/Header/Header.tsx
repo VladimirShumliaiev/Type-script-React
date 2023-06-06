4ea501cccc6120b5e782e8d3163e933394571a59
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-06-06 12:57:07 +0300

    05.06.2023 +++

--- my-app/src/Components/Header/Header.tsx
+++ my-app/src/Components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import React, {FC} from 'react';
 import style from './Header.module.css'
-import Menu from "./Header componnets/Menu";
 
 type Header = {
     theme: string
@@ -15,7 +14,6 @@ const Header: FC<Header> = ({theme, setTheme}) => {
 
     return (
         <div className={style.item}>
-            <Menu/>
                 <input className={style.inputHeader} placeholder={'add text...'} />
                 <button className={style.search}><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/OOjs_UI_icon_search-ltr.svg/1200px-OOjs_UI_icon_search-ltr.svg.png" alt=""/></button>
             <button onClick={onclickHandler} className={style.themeButton}>theme</button>

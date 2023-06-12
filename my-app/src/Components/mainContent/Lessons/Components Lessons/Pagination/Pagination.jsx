dd0cafafdbca992ca4f82d5e0e6333925c48a05b
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-06-12 13:30:32 +0300

    09.06.2023 +++

--- my-app/src/Components/mainContent/Lessons/Components Lessons/Pagination/Pagination.jsx
+++ my-app/src/Components/mainContent/Lessons/Components Lessons/Pagination/Pagination.jsx
@@ -1,36 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import axios from "axios";
 import style from './Pagination.module.css'
-import Countries from "./Countries";
-import PaginationItem from "./PaginationItem";
+import axios from "axios";
 
 const Pagination = () => {
-    const [countries, setCountries] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [currentPage, setCurrentPage] = useState(1)
-    const [countriesPerPage, setCountriesPerPage] = useState(10)
-
+    const [images, setImages] = useState([])
 
     useEffect(() => {
-        axios.get('https://restcountries.com/v3.1/all')
-            .then(res => {
-                setLoading(true)
-                setCountries(res.data)
-                console.log(res.data)
-                setLoading(false)
-            })
-    }, [])
-
-    const lastCountiesIndex = currentPage * countriesPerPage
-    const firstCountiesIndex = lastCountiesIndex - countriesPerPage
-    const currentCounties = countries.slice(firstCountiesIndex, lastCountiesIndex)
-
-    const paginate = pageNumber => setCurrentPage(pageNumber)
+        axios.get('https://jsonplaceholder.typicode.com/albums/1/photos')
+            .then(res => setImages(res.data))
+    },[])
 
     return (
-        <div>
-           <Countries countries={currentCounties} loading={loading}/>
-            <PaginationItem paginate={paginate} countriesPerPage={countriesPerPage} totalCounties={countries.length}/>
+        <div className={style.item}>
+            {images.map((e, index) => <h3 key={index}><img src={e.url} alt=""/></h3>)}
         </div>
     );
 };

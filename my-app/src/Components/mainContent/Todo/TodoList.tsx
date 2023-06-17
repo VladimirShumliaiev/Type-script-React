6d685cff71528c72a30d99cbbd185fc9c3d115ef
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-06-17 21:44:45 +0300

    09.06.2023 +++

--- my-app/src/Components/mainContent/Todo/TodoList.tsx
+++ my-app/src/Components/mainContent/Todo/TodoList.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useAppSelector} from "../../../hooks/hooks";
 import TodoListItem from "./TodoListItem";
+import ReactPaginate from "react-paginate";
+import style from './Todo.module.css'
 
 const TodoList = () => {
     const selector = useAppSelector(state => state.todo.todoList)
+    const [currentItems, setCurrentItems] = useState(selector);
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 10
+
+    useEffect(() => {
+        const endOffset = itemOffset + itemsPerPage;
+        setCurrentItems(selector.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(selector.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage,selector]);
+
+
+    const handlePageClick = (event: any) => {
+        const newOffset = event.selected * itemsPerPage % selector.length;
+        setItemOffset(newOffset);
+    };
     return (
         <div>
             {
-                selector.map(e => <TodoListItem key={e.id} {...e}/>)
+                currentItems.map(e => <TodoListItem key={e.id} {...e}/>)
             }
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                previousLabel="< previous"
+                renderOnZeroPageCount={null}
+                containerClassName={style.Pagination}
+                pageLinkClassName={style.pageNum}
+                previousLinkClassName={style.pageNum}
+                nextLinkClassName={style.pageNum}
+                activeLinkClassName={style.active}
+            />
         </div>
     );
 };
--- my-app/src/redux/Slices/todoSlice.ts
+++ my-app/src/redux/Slices/todoSlice.ts
@@ -22,7 +22,7 @@ const initialState: TodoState = {
 export const fetchTodo = createAsyncThunk<Todo[], undefined, { rejectValue: string }>(
     'Todo/fetchTodo',
     async (_, {rejectWithValue}) => {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/?_limit=15')
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
 
         if (!response) {
             return rejectWithValue('Error fetch todo')

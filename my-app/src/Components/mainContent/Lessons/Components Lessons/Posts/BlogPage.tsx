02d472c9e5cff4b5280159f54938562d9386ce03
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-07-01 12:14:39 +0300

    01.07.2023+++

--- my-app/src/Components/mainContent/Lessons/Components Lessons/Posts/BlogPage.tsx
+++ my-app/src/Components/mainContent/Lessons/Components Lessons/Posts/BlogPage.tsx
@@ -2,9 +2,9 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {Link} from "react-router-dom";
 import ReactPaginate from "react-paginate";
-import style from './Posts.module.css'
+import style from './Posts.module.css';
 
-type Post = {
+export type Posts = {
     id: string
     title: string
     url: string
@@ -12,11 +12,11 @@ type Post = {
 }
 
 const BlogPage = () => {
-    const [posts, setPosts] = useState<Post[]>([])
+    const [posts, setPosts] = useState<Posts[]>([])
     const [currentItems, setCurrentItems] = useState(posts);
     const [pageCount, setPageCount] = useState(0);
     const [itemOffset, setItemOffset] = useState(0);
-    const itemsPerPage = 25
+    const itemsPerPage = 10;
 
     useEffect(() => {
         const endOffset = itemOffset + itemsPerPage;
@@ -24,11 +24,11 @@ const BlogPage = () => {
         setPageCount(Math.ceil(posts.length / itemsPerPage));
     }, [itemOffset, itemsPerPage, posts]);
 
-
     const handlePageClick = (event: any) => {
         const newOffset = event.selected * itemsPerPage % posts.length;
         setItemOffset(newOffset);
     };
+
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/photos').then
@@ -36,25 +36,26 @@ const BlogPage = () => {
     },[])
 
     return (
-        <div>
-            {currentItems.map(e => (
-                <li>
-                    <Link key={e.id} to={`/lessons/posts/${e.id}`}>{e.title}</Link>
-                </li>
+        <div className={style.item}>
+            {currentItems.map(post => (
+                <div>
+                    <Link to={`/lessons/posts/${post.id}`}>
+                        {post.title}
+                    </Link>
+                </div>
             ))}
             <ReactPaginate
+                breakLabel="..."
                 nextLabel="next >"
                 onPageChange={handlePageClick}
-                pageRangeDisplayed={3}
-                marginPagesDisplayed={1}
+                pageRangeDisplayed={5}
                 pageCount={pageCount}
                 previousLabel="< previous"
+                renderOnZeroPageCount={null}
+                containerClassName={style.pagination}
                 pageLinkClassName={style.pageNum}
                 previousLinkClassName={style.pageNum}
                 nextLinkClassName={style.pageNum}
-                breakLabel="..."
-                containerClassName={style.pagination}
-                activeClassName={style.active}
                 activeLinkClassName={style.active}
             />
         </div>
--- my-app/src/Components/mainContent/Lessons/Components Lessons/Posts/SinglePage.tsx
+++ my-app/src/Components/mainContent/Lessons/Components Lessons/Posts/SinglePage.tsx
@@ -1,34 +1,35 @@
 import React, {useEffect, useState} from 'react';
+import {Posts} from "./BlogPage";
 import axios from "axios";
 import {Link, useParams} from "react-router-dom";
 
-type Post = {
-    id: string
-    title: string
-    url: string
-    thumbnailUrl: string
-}
-
 const SinglePage = () => {
-    const [posts, setPosts] = useState<Post>()
+    const [posts, setPosts] = useState<Posts>()
     const {id} = useParams()
 
     useEffect(() => {
         axios.get(`https://jsonplaceholder.typicode.com/photos/${id}`).then
         (({data}) => setPosts(data))
-    },[])
+    }, [])
 
     return (
         <div>
             {posts && (
                 <div>
-                    <Link to={'/lessons/posts'}><h3>back</h3></Link>
+                    <div>
+                        <Link to={'/lessons/posts'}>
+                            <h3>
+                            BACK
+                        </h3>
+                        </Link>
+                    </div>
                     {posts.title}
                     <div>
                         <img src={posts.url} alt=""/>
-                        <div>
-                            <img src={posts.thumbnailUrl} alt=""/>
-                        </div>
+                    </div>
+
+                    <div>
+                        <img src={posts.thumbnailUrl} alt=""/>
                     </div>
 
                 </div>
